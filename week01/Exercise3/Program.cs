6576075cfbc6b5f570134debbc52c9b7b880aadb
brauliovilla <bvillamiranda@byupathway.edu>
2025-07-13 16:32:33 -0500

    Project finished

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -2;
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string inpt = Console.ReadLine();
+            guess = int.Parse(inpt);
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+       
     }
 }
